fcdc6b809b1e54b2f0283e6f27f4f0a30fffa314
Neev <groverneev@gmail.com>
2026-03-18 20:09:38 -0700

    added to programs

--- Cpp/HelloWorld.cpp
+++ Cpp/HelloWorld.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, Neev!" << endl; // if you didn't have the line using namespace std, you would have to write:
-    // std::cout ... << std::endl
+    cout << "Hello, Neev!\n"; // if you didn't have the line using namespace std, you would have to write:
+    // std::cout "";
+
+    /*
+    This is a comment block
+    Everything inside this is commented
+    */
+
+    cout << 1.234 << "\n";
+    cout << 4 << "Temp\n";
+    int x = 0;
+    cout << x;
     return 0;
 }
